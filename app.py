fea1b3a8d4fe3e71189c89525174b88b4dae4626
daniel <daniel@yavorovych.com>
2018-04-19 17:52:06 +0300

    refactoring

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 import json
-import datetime
 import logging
-from flask import Flask, abort
-
 import requests
+import datetime
+from flask import Flask, abort
 from web3 import Web3, HTTPProvider
+
 import settings
 
-GETH_POD_HOSTNAME_REGEX = r'^(.*)-geth-\d+-.*$'
+# Init
+logging.basicConfig(level=logging.INFO)
 
 ETHERSCAN_API_URLS = {
     1: 'https://api.etherscan.io/api',
@@ -16,12 +17,25 @@ ETHERSCAN_API_URLS = {
     4: 'http://rinkeby.etherscan.io/api',
 }
 
+DB = {
+    'START_TIME': datetime.datetime.now(),
+    'LAST_BLOCK': 0,
+    'ETHERSCAN_API_URL': None
+}
+
+GETH_URL = 'http://localhost:%s' % settings.ETH_RPC_PORT
+
+app = Flask(__name__)
+
+w3_http_provider = HTTPProvider(GETH_URL, request_kwargs={'timeout': settings.ETH_RPC_TIMEOUT})
+w3_client = Web3(w3_http_provider)
+
 
 def get_eth_net_version(w3):
     result = json.loads(w3.currentProvider.make_request('net_version', []))
     version = result.get('result')
     if not version:
-        logging.error('Error getting ethereum version. Using main net as default')
+        logging.error('Error getting ethereum network version. Using main net as default')
         return 1
 
     return int(version)
@@ -29,11 +43,12 @@ def get_eth_net_version(w3):
 
 def get_etherscan_api_url(version):
     url = ETHERSCAN_API_URLS.get(version)
+
     if not url:
         raise BaseException('Unsupported ethereum network')
 
-    logging.info(
-        'Detect ethereum network version "{version}" and Etherscan API url "{url}"'.format(version=version, url=url))
+    logging.info('Detect ethereum network version "{version}" and Etherscan API url "{url}"'.format(version=version,
+                                                                                                    url=url))
     return url
 
 
@@ -42,10 +57,8 @@ def get_etherscan_highest_block(url):
                       'action': 'eth_blockNumber',
                       'apikey': settings.ETHERSCAN_API_KEY}
 
-    response = requests.get(url, params=request_params)
-    response_json = response.json()
-    highest_block_hex = response_json.get('result')
-    highest_block = int(highest_block_hex, 0)
+    data = requests.get(url, params=request_params).json()
+    highest_block = int(data.get('result'), 0)
 
     return highest_block
 
@@ -55,34 +68,12 @@ def get_eth_sync_diff(w3, ethercan_api_url):
     current_block = w3.eth.blockNumber
     sync_diff = highest_block - current_block
 
-    if sync_diff < 0:
-        return current_block, 0
-
     return current_block, sync_diff
 
 
-# Init
-logging.basicConfig(level=logging.INFO)
-
-app = Flask(__name__)
-
-geth_url = 'http://localhost:%s' % settings.ETH_RPC_PORT
-w3_http_provider = HTTPProvider(geth_url, request_kwargs={'timeout': settings.ETH_RPC_TIMEOUT})
-w3_client = Web3(w3_http_provider)
-
-DB = {
-    'START_TIME': datetime.datetime.now(),
-    'LAST_BLOCK': 0,
-    'ETHERSCAN_API_URL': None
-}
-
-
+# Flask routes
 @app.route("/healthz")
 def liveness():
-    if datetime.datetime.now() - DB['START_TIME'] <= datetime.timedelta(seconds=settings.START_WAIT_TIME):
-        logging.info('Waiting start time period (%s sec), passing check' % settings.START_WAIT_TIME)
-        return 'starting...'
-
     try:
         if not DB['ETHERSCAN_API_URL']:
             DB['ETHERSCAN_API_URL'] = get_etherscan_api_url(get_eth_net_version(w3_client))
@@ -96,7 +87,7 @@ def liveness():
         logging.error('Node not syncing. Diff: %s' % sync_diff)
         abort(500)
 
-        DB['LAST_BLOCK'] = current_block
+    DB['LAST_BLOCK'] = current_block
 
     logging.info('Node is synced')
 
--- settings.py
+++ settings.py
@@ -1,14 +1,9 @@
 import os
 
-# How long waiting before check after start (in seconds)
-START_WAIT_TIME = int(os.environ.get('START_WAIT_TIME', 120))
-
-# Ethereum
 ETH_RPC_PORT = int(os.environ.get('ETH_RPC_PORT', 8545))
 ETH_RPC_TIMEOUT = int(os.environ.get('ETH_RPC_TIMEOUT', 30))
 ETH_MAX_SYNC_DIFF = int(os.environ.get('ETH_MAX_SYNC_DIFF', 50))
 ETHERSCAN_API_KEY = os.environ.get('ETHERSCAN_API_KEY')
-
 
 try:
     from local_settings import *
